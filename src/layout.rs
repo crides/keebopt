7e247d882169d87707603f8c0668d028e39963bc
crides <zhuhaoqing@live.cn>
2020-12-05 02:05:00 -0600

    Changes to generic keys layout, add one more finger, change some multipliers

--- src/layout.rs
+++ src/layout.rs
@@ -1,95 +1,125 @@
-#![allow(dead_code)]
+use std::collections::{BTreeMap, BTreeSet};
 
-use std::collections::BTreeMap;
+use smallvec::{smallvec, SmallVec};
 
 pub type FreqsData = Vec<(String, usize)>;
 
 #[derive(Clone, Debug)]
-pub struct Layout(pub BTreeMap<u8, char>);
+pub struct Layout(pub BTreeMap<Chord, char>);
 
-#[derive(Clone, Debug)]
-pub struct ReverseLayout(pub BTreeMap<char, u8>);
+type ChordStore = SmallVec<[u8; 4]>;
 
-pub struct Chord(pub Vec<u8>);
+#[derive(Clone, Debug, Ord, PartialOrd, PartialEq, Eq)]
+pub struct Chord(pub ChordStore);
 
 impl Layout {
-    pub fn shuffle(&mut self, n: usize) {
-        let mut maps: Vec<(u8, char)> = self.0.clone().into_iter().collect();
-        for _ in 0..n {
-            loop {  // TODO
-                let (i, j) = (
-                    rand::random::<usize>() % maps.len(),
-                    rand::random::<usize>() % (maps.len() - 1),
-                );
-                let j = if j >= i { j + 1 } else { j };
-                let (mut a, mut b) = (maps[i], maps[j]);
-                if a.1 == ' ' && b.1 == ' ' {
-                    continue;
+    pub const KEYS: &'static [u8] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    pub const KEYS_COST: &'static [f64] = &[3.2, 1.0, 1.1, 1.4, 2.3, 3.2, 1.0, 1.1, 1.4, 2.3];
+    pub const KEYS_NUM: usize = Layout::KEYS.len();
+    pub const CHORD_NUM_POS: usize = Layout::KEYS_NUM + Layout::KEYS_NUM * (Layout::KEYS_NUM - 1) / 2 - 2;
+    pub fn init() -> Layout {
+        let mut target_chars: Vec<char> = (0..Layout::CHORD_NUM_POS)
+            .map(|i| {
+                if i < 26 {
+                    (i as u8 + 'a' as u8) as char
+                } else {
+                    ' '
                 }
-                std::mem::swap(&mut a.1, &mut b.1);
-                maps[i] = a;
-                maps[j] = b;
-                break;
-            }
-        }
-        self.0 = maps.into_iter().collect();
-    }
-
-    fn _cost(finger: u8) -> f64 {
-        match finger {
-            8 => 1.0,
-            4 => 1.1,
-            2 => 1.4,
-            1 => 2.3,
-            0 => 0.0,
-            _ => panic!("{:02x}", finger),
-        }
+            })
+            .collect();
+        Layout(
+            (0..Layout::KEYS_NUM)
+                .map(|i| smallvec![Layout::KEYS[i]])
+                .chain((0..Layout::KEYS_NUM).flat_map(|i| {
+                    ((i + 1)..Layout::KEYS_NUM)
+                        .filter_map(|j| {
+                            if i == 0 && j == 6 || i == 1 && j == 5 {
+                                None
+                            } else {
+                                Some(smallvec![Layout::KEYS[i], Layout::KEYS[j]])
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                }))
+                .map(|v| {
+                    let ind = rand::random::<usize>() % target_chars.len();
+                    (Chord(v), target_chars.remove(ind))
+                })
+                .collect(),
+        )
     }
 
     fn cost(finger: u8) -> f64 {
-        Layout::_cost(finger & 0xF) + Layout::_cost(finger >> 4)
+        Layout::KEYS_COST[finger as usize]
     }
 
-    fn chord_cost(chord: u8) -> f64 {
-        if chord.count_ones() == 1 {
-            Layout::cost(chord)
+    fn chord_cost(chord: &Chord) -> f64 {
+        if chord.0.len() == 1 {
+            Layout::cost(chord.0[0])
         } else {
-            let top = ((chord as usize).next_power_of_two() / 2) as u8;
-            let bottom = chord & !top;
-            let (a, b) = (Layout::cost(top), Layout::cost(bottom));
+            let (big, small) = if chord.0[0] > chord.0[1] {
+                (chord.0[0], chord.0[1])
+            } else {
+                (chord.0[1], chord.0[0])
+            };
+            let (a, b) = (Layout::cost(big), Layout::cost(small));
             (a + b)
-                * if top >> 4 == bottom {
+                * if big - 5 == small {
                     1.5
-                } else if top >= 0x10 && bottom >= 0x10 || top < 0x10 && bottom < 0x10 {
-                    1.2
-                } else if top == 8 && bottom == 4
-                    || bottom == 4 && top == 2
-                    || bottom == 2 && top == 1
+                } else if big >= 5 && small >= 5 || big < 5 && small < 5 {
+                    1.0
+                } else if small == 1 && big == 7 || big == 7 && small == 3 || big == 8 && small == 4
                 {
-                    1.7
+                    1.9
                 } else {
                     1.3
                 }
         }
     }
 
+    fn consec_cost(old: &Chord, new: &Chord) -> f64 {
+        old.0
+            .iter()
+            .map(|l| {
+                new.0
+                    .iter()
+                    .map(|c| {
+                        let (small, big) = if c < l {
+                            (*c, *l)
+                        } else {
+                            (*l, *c)
+                        };
+                        if small == big {
+                            0.5 * Layout::cost(small)
+                        } else if small == big - 5 {
+                            Layout::cost(small)
+                        } else if small == 0 && big == 6 || small == 1 && big == 5 {
+                            (Layout::cost(1) + Layout::cost(5)) / 2.0 * 1.414
+                        } else if small == 0 && big == 1 || small == 5 && big == 6 {
+                            (Layout::cost(1) + Layout::cost(5)) / 2.0
+                        } else {
+                            0.0
+                        }
+                    })
+                    .sum::<f64>()
+            })
+            .sum()
+    }
+
     pub fn total_cost(&self, data: &FreqsData) -> f64 {
-        let char_map: BTreeMap<char, u8> = self.0.iter().map(|(k, v)| (*v, *k)).collect();
+        let char_map: BTreeMap<char, Chord> = self.0.iter().map(|(k, v)| (*v, k.clone())).collect();
         let mut cost = 0.0f64;
         for (s, count) in data.iter() {
-            let mut last_chord = None;
+            let mut last_chord: Option<Chord> = None;
             cost += (*count as f64)
                 * s.chars()
                     .map(|c| {
-                        let chord = char_map[&c.to_ascii_lowercase()];
+                        let chord = &char_map[&c.to_ascii_lowercase()];
                         let mut cost = Layout::chord_cost(chord);
-                        if let Some(last) = last_chord {
-                            let common = last & chord;
-                            if common != 0 {
-                                cost += Layout::chord_cost(common)
-                            }
+                        if let Some(last) = &last_chord {
+                            cost += Layout::consec_cost(last, chord);
                         }
-                        last_chord = Some(chord);
+                        last_chord = Some(chord.clone());
                         cost
                     })
                     .sum::<f64>();
@@ -97,37 +127,48 @@ impl Layout {
         cost
     }
 
-    fn repr_chord(chord: u8) -> String {
-        (0..4)
-            .rev()
-            .map(|i| {
-                let dyad = (chord >> (i + 3)) & 0x02 | (chord >> i) & 0x01;
+    fn repr_chord(chord: Chord) -> String {
+        let keys: BTreeSet<u8> = chord.0.into_iter().collect();
+        [(0, 5), (1, 6), (2, 7), (3, 8), (4, 9)]
+            .iter()
+            .map(|(top, bottom)| {
+                let dyad = (keys.contains(top), keys.contains(bottom));
                 match dyad {
-                    0 => " ",
-                    1 => "▄",
-                    2 => "▀",
-                    3 => "█",
-                    _ => panic!("!!"),
+                    (false, false) => " ",
+                    (false, true) => "▄",
+                    (true, false) => "▀",
+                    (true, true) => "█",
                 }
             })
             .collect()
     }
 
-    fn repr_mapping(chord: u8, c: char) -> String {
+    fn repr_mapping(chord: Chord, c: char) -> String {
         format!("|{}|: {}", Layout::repr_chord(chord), c)
     }
 
-    pub fn print(&self) {
-        let mut maps: Vec<(u8, char)> = self.0.clone().into_iter().filter(|p| p.1 != ' ').collect();
+    pub fn repr_layout(&self) -> String {
+        let mut maps: Vec<(Chord, char)> =
+            self.0.clone().into_iter().filter(|p| p.1 != ' ').collect();
         maps.sort_by_key(|e| {
             (
-                e.0.count_ones(),
-                -((e.0 as usize).next_power_of_two() as isize),
+                if e.0 .0.len() > 1 { 1 } else { 0 },
+                e.0 .0.iter().copied().max().unwrap(),
             )
         });
-        for chunk in maps.chunks(8) {
-            let reprs = chunk.iter().map(|p| Layout::repr_mapping(p.0, p.1)).collect::<Vec<_>>();
-            println!("{}", reprs.join(", "));
-        }
+        maps.chunks(Layout::KEYS_NUM)
+            .map(|chunk| {
+                let reprs = chunk
+                    .iter()
+                    .map(|p| Layout::repr_mapping(p.0.clone(), p.1))
+                    .collect::<Vec<_>>();
+                reprs.join(", ")
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    pub fn print(&self) {
+        println!("{}", self.repr_layout());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate serde_derive;
 
-mod annealing;
+// mod annealing;
 mod layout;
 
 use std::collections::BTreeMap;
 
 use indicatif::{ProgressBar, ProgressStyle};
 
-use layout::{Layout, FreqsData};
+use layout::{Chord, FreqsData, Layout};
 
 pub fn progress_bar(len: usize, msg: &str) -> ProgressBar {
     let pbar = ProgressBar::new(len as u64).with_style(
@@ -21,56 +21,74 @@ pub fn progress_bar(len: usize, msg: &str) -> ProgressBar {
 }
 
 fn main() {
-    let mut layout: Layout = Layout({
-        let mut target_chars: Vec<char> = (0..36)
-            .map(|i| {
-                if i < 26 {
-                    (i as u8 + 'a' as u8) as char
-                } else {
-                    ' '
-                }
-            })
-            .collect();
-        (1..=255u8)
-            .filter(|i| i.count_ones() <= 2)
-            .enumerate()
-            .map(|(_i, k)| {
-                let ind = rand::random::<usize>() % target_chars.len();
-                (k, target_chars.remove(ind))
-            })
-            .collect()
-    });
     let freqs_data: FreqsData =
         serde_json::from_str::<BTreeMap<String, usize>>(include_str!("../ngrams-all.json"))
             .unwrap()
             .into_iter()
             .collect();
-    let init_cost = layout.total_cost(&freqs_data);
-    let mut cost = init_cost;
 
-    let pbar = progress_bar(annealing::N, "annealing");
-    pbar.set_draw_delta(annealing::N as u64 / 100);
-    let mut layouts = Vec::new();
-    for i in 1..(annealing::N + 1) {
-        let mut new_layout = layout.clone();
-        let times = rand::random::<usize>() % 3 + 1;
-        new_layout.shuffle(times);
-        let new_cost = new_layout.total_cost(&freqs_data);
-        if annealing::accept_transition(new_cost - cost, i) {
-            cost = new_cost;
-            layout = new_layout;
-            layouts.push((layout.clone(), cost));
+    let mut target = 30000000000000.0f64;
+    let mut lowest_count = 0;
+    loop {
+        let mut layout = Layout::init();
+        let init_cost = layout.total_cost(&freqs_data);
+        let mut cost = init_cost;
+
+        for cycle in 0.. {
+            let maps = {
+                let mut maps: Vec<(Chord, char)> =
+                    layout.0.iter().map(|(ch, &c)| (ch.clone(), c)).collect();
+                maps.sort_by_key(|p| if p.1 == ' ' { 1 } else { 0 });
+                maps
+            };
+            let mut new_layouts = (0usize..26)
+                .flat_map(|i| {
+                    ((i + 1)..Layout::CHORD_NUM_POS)
+                        .map(|j| {
+                            let mut new_maps = maps.clone();
+                            let old_char = new_maps[i].1;
+                            new_maps[i].1 = new_maps[j].1;
+                            new_maps[j].1 = old_char;
+                            let new_layout = Layout(new_maps.into_iter().collect());
+                            let new_cost = new_layout.total_cost(&freqs_data);
+                            (new_layout, new_cost)
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            new_layouts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+            let old_cost = cost;
+            let best = new_layouts.into_iter().next().unwrap();
+            cost = best.1;
+            layout = best.0;
+            if cost >= old_cost {
+                println!("cycle = {}, cost: {}", cycle, cost);
+                break;
+            }
         }
-        if i % 100 == 0 && layouts.len() > 20 {
-            layouts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-            layouts.drain(20..);
+        if cost < target {
+            println!("cost: {}, ratio: {}", cost, cost / init_cost);
+            layout.print();
+            target = cost;
+            println!("==============================");
+            println!("||  New Target: {}", target);
+            println!("==============================");
+            let mesg = format!("cost: {}\n{}", cost, layout.repr_layout());
+            std::process::Command::new("notify-send")
+                .args(&["keebopt", &mesg, "-u", "critical"])
+                .spawn()
+                .unwrap();
+        // break;
+        } else if cost == target {
+            lowest_count += 1;
+            std::process::Command::new("notify-send")
+                .args(&["keebopt", "!!!!!!!!", "-u", "critical"])
+                .spawn()
+                .unwrap();
         }
-        pbar.inc(1);
-    }
-    pbar.finish_with_message("annealed");
-    println!("init cost: {}", init_cost);
-    for layout in layouts.iter().take(3) {
-        println!("cost: {}, ratio: {}", layout.1, layout.1 / init_cost);
-        layout.0.print();
+        if lowest_count > 0 {
+            // We've hit it more than 1 times
+            break;
+        }
     }
 }
